a279a9acce2130398280c3027d5f1d67e867e197
noelcastillon1 <noelcastillon1@gmail.com>
2023-10-22 22:45:51 -0400

    add navbar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,36 @@
+const NavBar = () => {
+  return (
+    <>
+      <div className="navbar-wrapper flex flex-row items-center place-content-between h-28 px-7">
+        <div className="navbar-logo">
+          <img
+            className="rounded-full"
+            src="http://placekitten.com/50/50"
+            alt="logo for Columbus Code & Coffee"
+          />
+        </div>
+        <div className="navbar-links">
+          <ul className="flex flex-row gap-14">
+            <li>
+              <a href="http://localhost:5173/">Home</a>
+            </li>
+            <li>
+              <a href="http://localhost:5173/">About</a>
+            </li>
+            <li>
+              <a href="http://localhost:5173/">Community Groups</a>
+            </li>
+            <li>
+              <a href="http://localhost:5173/">Get Involved</a>
+            </li>
+            <li>
+              <button>Contact</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;

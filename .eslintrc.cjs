4e271e3afd4beb6a8b0dcc2f77b78c2121ee4220
noelcastillon1 <noelcastillon1@gmail.com>
2024-09-18 19:32:25 -0400

    fix: now?

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,17 +6,19 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:prettier/recommended",
+    "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: [
+    "react-refresh",
+    "prettier",
+  ],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
+    "prettier/prettier": ["error"],
   },
-  eslintPluginPrettierRecommended,
 };

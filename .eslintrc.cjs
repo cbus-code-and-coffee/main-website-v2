b0f4b36d89cad867cc40f89c236e453285bc2d79
noelcastillon1 <noelcastillon1@gmail.com>
2024-09-18 18:00:32 -0400

    fix: this should work?

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
-    "prettier",
+    "plugin:prettier/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",

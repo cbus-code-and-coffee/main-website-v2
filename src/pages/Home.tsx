bfa646bd32878357a119c5249107d12ac37d4a25
Harshvardhan1012 <hs883532@gmail.com>
2024-10-27 00:12:56 +0530

    fix-about-us

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -32,17 +32,6 @@ const Home = () => {
             btnText={"Learn more"}
           />
         </Section>
-        <Section>
-          <Card
-            image={"https://dummyimage.com/600x400/eeeeee/fff"}
-            imageAlt={"Image"}
-            title={"About Us"}
-            pText={
-              "Lorem ipsum demo demoLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-            }
-            btnText={"Learn more"}
-          />
-        </Section>
       </>
       {/* <SiteForm title="Get in Touch" subtitle="Please contact us" /> */}
       <Footer />

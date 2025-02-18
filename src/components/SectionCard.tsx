a1a2733c63cd6a9af88da1a650cccdcbdf38f800
yenterefe <yenterefe@gmail.com>
2025-02-17 21:08:35 -0500

    Comment addressed

---
+++ src/components/SectionCard.tsx
@@ -0,0 +1,27 @@
+interface PropsSecionCard {
+  image: string;
+  imageStyle: string;
+  title: string;
+  blurb: string;
+  isButton?: boolean;
+  btnTitle?: string;
+}
+
+const SectionCard = (props: PropsSecionCard) => {
+  return (
+    <div>
+      <div className="flex justify-center p-6">
+        <img className={props.imageStyle} src={props.image} />
+      </div>
+      <div className="text-center p-1">
+        <h5>{props.title}</h5>
+      </div>
+      <div>
+        <p>{props.blurb}</p>
+        {props.isButton ? <button>{props.btnTitle}</button> : null}
+      </div>
+    </div>
+  );
+};
+
+export default SectionCard;

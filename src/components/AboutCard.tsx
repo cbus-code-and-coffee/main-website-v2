991b955acc798f8fe83b90dfdc1d369e1fd84b65
yenterefe <yenterefe@gmail.com>
2025-05-01 20:28:36 -0400

    fix: comment

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -3,32 +3,30 @@ const AboutCard = (props: {
   subTitle: string;
   pText: string;
   image: string;
-  // this boolean reverses the blurb and image so the component can be reused
   isButton: boolean;
   btnText?: string;
+  // this boolean reverses the blurb and image so the component can be reused
   isReversed: boolean;
 }) => {
   const order = props.isReversed ? "order-first" : "order-last";
   return (
-    <>
-      <div className="px-5">
-        <h3 className="text-2xl py-10 text-center">{props.title}</h3>
-        <div className="grid grid-cols-2 items-center">
-          <div>
-            <img className={`rounded-lg ${order}`} src={props.image} />
-          </div>
-          <div className={`p-3 ${order}`}>
-            <h3 className="text-2xl py-2">{props.subTitle}</h3>
-            <p className="text-base ">{props.pText}</p>
-            {props.isButton && (
-              <div className="card-btn-wrapper mt-8">
-                <button className="btn-blue-bg">{props.btnText}</button>
-              </div>
-            )}
-          </div>
+    <div className="px-5">
+      <h3 className="text-2xl py-10 text-center">{props.title}</h3>
+      <div className="grid grid-cols-2 items-center">
+        <div>
+          <img className={`rounded-lg ${order}`} src={props.image} />
+        </div>
+        <div className={`p-3 ${order}`}>
+          <h3 className="text-2xl py-2">{props.subTitle}</h3>
+          <p className="text-base ">{props.pText}</p>
+          {props.isButton && (
+            <div className="card-btn-wrapper mt-8">
+              <button className="btn-blue-bg">{props.btnText}</button>
+            </div>
+          )}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 

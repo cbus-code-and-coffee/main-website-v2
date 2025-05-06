804942730ca493f987d2bb0d5c14ea3021a8b15b
yenterefe <yenterefe@gmail.com>
2025-05-05 23:20:29 -0400

    feat: mobile responsiveness

--- src/components/SponsorCard.tsx
+++ src/components/SponsorCard.tsx
@@ -16,7 +16,9 @@ const SponsorCard: React.FC<PropsSponsor> = ({
   return (
     <div className="p-2">
       <h4 className="text-center p-3">{sponsorTier} Roast Tier Supporters</h4>
-      <div className="flex gap-4 justify-center flex-wrap">
+      <div
+        className={`${images.length === 1 ? "flex justify-center" : "grid grid-cols-2 gap-4 max-w-[400px] mx-auto lg:flex justify-center"}`}
+      >
         {images.map((source, index) => (
           <img
             key={index}
